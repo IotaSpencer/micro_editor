8f0772cd90e2949ce6ebfb0b0aac8c43280d1252
Ken Spencer <ken@electrocode.net>
2018-03-02 16:07:55 -0500

    update installation

--- lib/micro_install.rb
+++ lib/micro_install.rb
@@ -15,48 +15,56 @@ module MicroInstall
     end
     
     def latesttag(hl = @hl)
-      hl.say "#{Paint["Getting Latest Tag", 'green']}"
-      MicroInstall.show_wait_spinner {
-        body = Unirest.get('https://api.github.com/repos/zyedidia/micro/releases/latest').body
-        @tag = body['tag_name'].gsub(/^v/, '')
-      }
-      hl.say "#{Paint['Latest Tag', 'green']}: #{Paint[@tag, 'yellow']}"
+      begin
+        hl.say "#{Paint["Getting Latest Tag", 'green']}"
+        MicroInstall.show_wait_spinner {
+          body = Unirest.get('https://api.github.com/repos/zyedidia/micro/releases/latest').body
+          @tag = body['tag_name'].gsub(/^v/, '')
+        }
+        hl.say "#{Paint['Latest Tag', 'green']}: #{Paint[@tag, 'yellow']}"
+      rescue
+       hl.say "#{Paint['Error', 'red']}: Unable to retrieve latest release."
+      end
     end
     
     def get_arch(hl = @hl)
-      host_os = OS.config['host_os']
-      bits    = OS.bits.to_s
-      case host_os
-        when "linux-gnu"
-          if bits == "64"
-            @arch = 'linux64'
-          elsif bits == "32"
-            @arch = 'linux32'
-          end
-        when "linux-arm"
-          raise self.LookupError 'ARM (mobile devices) are not supported'
-        when "darwin"
-          @arch = 'osx'
-        when "freebsd"
-          if bits == "64"
-            @arch = 'freebsd64'
-          elsif bits == "32"
-            @arch = 'freebsd32'
-          end
-        when "openbsd"
-          if bits == "64"
-            @arch = 'openbsd64'
-          elsif bits == "32"
-            @arch = 'openbsd32'
-          end
-        when "netbsd"
-          if bits == "64"
-            @arch = 'netbsd64'
-          elsif bits == "32"
-            @arch = 'netbsd32'
-          end
-        else
-          raise self.LookupError 'unable to determine your system'
+      begin
+        host_os = OS.config['host_os']
+        bits    = OS.bits.to_s
+        case host_os
+          when "linux-gnu"
+            if bits == "64"
+              @arch = 'linux64'
+            elsif bits == "32"
+              @arch = 'linux32'
+            end
+          when "linux-arm"
+            raise self.LookupError 'ARM (mobile devices) are not supported'
+          when "darwin"
+            @arch = 'osx'
+          when "freebsd"
+            if bits == "64"
+              @arch = 'freebsd64'
+            elsif bits == "32"
+              @arch = 'freebsd32'
+            end
+          when "openbsd"
+            if bits == "64"
+              @arch = 'openbsd64'
+            elsif bits == "32"
+              @arch = 'openbsd32'
+            end
+          when "netbsd"
+            if bits == "64"
+              @arch = 'netbsd64'
+            elsif bits == "32"
+              @arch = 'netbsd32'
+            end
+          else
+            raise self.LookupError 'unable to determine your system'
+        end
+      rescue MicroInstall::LookupError => e
+        hl.say "#{Paint['Error', 'red']}: #{e}"
       end
     
     end
@@ -67,7 +75,7 @@ module MicroInstall
     end
     
     def download_micro_tar(hl = @hl)
-      "Downloading"
+      hl.say "Downloading... "
       MicroInstall.show_wait_spinner {
         File.open("micro-#{@tag}-#{@arch}.tar.gz", "wb") do |saved_file|
           # the following "open" is provided by open-uri
@@ -78,26 +86,32 @@ module MicroInstall
       }
     end
     
-    def extract_micro
-      micro_tar = Gem::Package::TarReader.new(Zlib::GzipReader.open("micro-#{@tag}-#{@arch}.tar.gz"))
-      micro_tar.rewind
-      dest = nil
-      micro_tar.each do |entry|
-        dest ||= Pathname(Dir.home).realdirpath.join(entry.full_name).to_path
-        if entry.directory?
-          File.delete dest if File.file? dest
-          FileUtils.mkdir_p dest, :mode => entry.header.mode, :verbose => false
-        elsif entry.file?
-          FileUtils.rm_rf dest if File.directory? dest
-          File.open dest, "wb" do |f|
-            f.print entry.read
-          end
-          FileUtils.chmod entry.header.mode, dest, :verbose => false
-        elsif entry.header.typeflag == '2' #Symlink!
-          File.symlink entry.header.linkname, dest
-        end
+    def extract_micro(hl = @hl)
+      begin
+        micro_tar = Gem::Package::TarReader.new(Zlib::GzipReader.open("micro-#{@tag}-#{@arch}.tar.gz"))
+        micro_tar.rewind
         dest = nil
+        micro_tar.each do |entry|
+          dest ||= Pathname(Dir.home).realdirpath.join(entry.full_name).to_path
+          if entry.directory?
+            File.delete dest if File.file? dest
+            FileUtils.mkdir_p dest, :mode => entry.header.mode, :verbose => false
+          elsif entry.file?
+            FileUtils.rm_rf dest if File.directory? dest
+            File.open dest, "wb" do |f|
+              f.print entry.read
+            end
+            FileUtils.chmod entry.header.mode, dest, :verbose => false
+          elsif entry.header.typeflag == '2' #Symlink!
+            File.symlink entry.header.linkname, dest
+          end
+          dest = nil
+        end
+      rescue
+        hl.say "#{Paint['Error']}: Could not extract micro due to an error."
+      
       end
+    
     end
     
     def install_micro(hl = @hl)
@@ -121,13 +135,13 @@ module MicroInstall
       end
       
       begin
-        FileUtils.remove("micro-#{@tag}-#{@arch}.tar.gz")
+        FileUtils.rm("micro-#{@tag}-#{@arch}.tar.gz")
       rescue Errno::ENOENT => e
         hl.say "#{Paint['Error', 'red']}: #{e}"
       end
       
       begin
-        FileUtils.rmtree("micro-#{@tag}")
+        FileUtils.rm_r(Pathname(Dir.home).join("micro-#{@tag}"), :secure => true)
       rescue Errno::ENOENT => e
         hl.say "#{Paint['Error', 'red']}: #{e}"
       end
